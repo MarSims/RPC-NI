db21c57cbe0e103563eb882d1ec3393c284c62fa
MarSims <67733749+MarSims@users.noreply.github.com>
2024-11-09 18:16:01 +0800

    RPC_NI_demo.m

--- RPC_NI_demo.m
+++ RPC_NI_demo.m
@@ -1,29 +1,44 @@
 function RPC_NI_demo
-    %% load dataset
-     % clc; clear all;
-    data = textread('Jain.txt');
-    [N,dim]=size(data);
-    Tclass = data(:,dim);  % 真实类别
-    nClu = length(unique(Tclass));  %类别数
-    data = data(:,1:dim-1);   % 去除标签 
-    
-    %% parameter
-    % MinSize = 0.02*N; % Note: parameter MinSize (i.e.,the minimal cluster size) is dependent on ratio; 
-    MinSize = N/(2*nClu); % For equal size datasets, e.g., UCI datasets
-    
-    %% clustering
-    [Label] = RPC_NI(data, nClu, MinSize); %% nC: number of clusters;
-    
-    %% result
-    numClust = length(unique(Label));
-    disp('Number of Final Cluster');disp(numClust);
-    
-    nmi = compute_nmi (Label,Tclass);
-    disp('NMI:');disp(nmi);
-    
-    %% plot
-    figure;
-    clr = hsv(nClu*1);
-    gscatter(data(:,1),data(:,2),Tclass, clr, '.', 12);
+% -------------------------------------------------------------------------
+% Aim:
+% The matlab code of "Representative Point-Based Clustering with Neighborhood
+% Information for Complex Data Structures"
+% -------------------------------------------------------------------------
+% Input:
+% data: the data set
+% clu_num:the number of clusters
+% eta: cluster size parameter
+% -------------------------------------------------------------------------
+% Output:
+% abel: the clustering result of local density peaks
+% -------------------------------------------------------------------------
+% Written by Zhongju Shang, Haowei Wang
+% Novermber 2024
+
+%% load dataset
+ % clc; clear all;
+data = textread('Jain.txt');
+[N,dim]=size(data);
+Tclass = data(:,dim);  % 真实类别
+nClu = length(unique(Tclass));  %类别数
+data = data(:,1:dim-1);   % 去除标签 
+
+%% parameter
+eta = 0.02*N;       % For most cases (eta controls the minimum cluster size); 
+% eta = N/(2*nClu); % For equal size datasets, e.g., UCI datasets
+
+%% clustering
+[Label] = RPC_NI(data, nClu, eta); %% nC: number of clusters;
+numClust = length(unique(Label));
+disp('Number of Final Cluster');disp(numClust);
+
+%% results evaluation
+nmi = compute_nmi (Label,Tclass);
+disp('NMI:');disp(nmi);
+
+%% plot
+figure;
+clr = hsv(nClu*1);
+gscatter(data(:,1),data(:,2),Tclass, clr, '.', 12);
 
 end
